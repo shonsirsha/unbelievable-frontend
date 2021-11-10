4892c3df5e080ae28244ae3192c5d91e1247e64a
shonsirsha <seangeekpro@gmail.com>
2021-11-10 14:06:32 +0100

    finished policies

--- next.config.js
+++ next.config.js
@@ -13,24 +13,33 @@ module.exports = {
 								"https://checkout-staging.xendit.com",
 								"https://checkout.xendit.com",
 							],
+							styleSrc: ["'self'", "'unsafe-inline'"],
+							imgSrc: [
+								"'self'",
+								"unbelievable-webapp.s3.ap-southeast-1.amazonaws.com",
+								"https://vz-b4f1e97e-483.b-cdn.net",
+							],
+							baseUri: "self",
 						},
 					},
-					frameGuard: false,
 					forceHTTPSRedirect: [
 						true,
-						{ maxAge: 60 * 60 * 24 * 7, includeSubDomains: true },
+						{ maxAge: 63072000, includeSubDomains: true },
 					],
+					frameGuard: "deny",
+					noopen: "noopen",
+					nosniff: "nosniff",
 					xssProtection: "sanitize",
-					// referrerPolicy: "same-origin",
+					referrerPolicy: "strict-origin-when-cross-origin",
 				}),
 			},
 		];
 	},
 	// images: {
-	// 	minimumCacheTTL: 1400,
-	// 	domains: [
-	// 		"unb-dev.s3.ap-southeast-1.amazonaws.com",
-	// 		"unbelievable-webapp.s3.ap-southeast-1.amazonaws.com",
-	// 	],
+	//  minimumCacheTTL: 1400,
+	//  domains: [
+	//      "unb-dev.s3.ap-southeast-1.amazonaws.com",
+	//      "unbelievable-webapp.s3.ap-southeast-1.amazonaws.com",
+	//  ],
 	// },
 };
